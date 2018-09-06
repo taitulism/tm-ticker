b119e58fc032c3f691c5867a73d822dbf3b1873e
Itai Tenenbaum <taitulism@gmail.com>
2018-09-07 02:27:45 +0300

    Update eslint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -172,7 +172,9 @@ module.exports = {
         "no-trailing-spaces": "error",
         "no-undef-init": "error",
         "no-undefined": "error",
-        "no-underscore-dangle": "error",
+        "no-underscore-dangle": ["error", {
+			"allowAfterThis": true
+		}],
         "no-unmodified-loop-condition": "error",
         "no-unneeded-ternary": "error",
         "no-unused-expressions": ["error", {
